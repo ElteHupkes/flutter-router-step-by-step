a23b3dd9b9dd95d301dbdac557bfe6d126196d25
Elte Hupkes <elte@hupkes.org>
2022-09-04 15:22:51 +0200

    Step 3 completed

--- lib/router/delegate.dart
+++ lib/router/delegate.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:router_step_by_step/app_state.dart';
 
@@ -18,25 +19,60 @@ class AppRouterDelegate extends RouterDelegate<AppState> with ChangeNotifier {
 
   @override
   Widget build(BuildContext context) {
-    if (_state.postId != null) {
-      final post = posts.firstWhere((element) => element.id == _state.postId);
-      return PostDetailsScreen(
-        post: post,
-        update: changeAppState,
-      );
-    }
+    return Navigator(
+      onPopPage: _onPopPage,
+      pages: [
+        // The welcome screen is always there, at the bottom of the stack
+        MaterialPage(
+          child: WelcomeScreen(
+            update: changeAppState,
+          ),
+          name: "welcome",
+        ),
 
-    if (_state.showPosts) {
-      return PostsScreen(update: changeAppState);
-    }
+        // Display the post list if a post is showing, or if we
+        // requested the post list.
+        if (_state.showPosts || _state.postId != null)
+          MaterialPage(
+            child: PostsScreen(
+              update: changeAppState,
+            ),
+            name: "posts",
+          ),
 
-    return WelcomeScreen(update: changeAppState);
+        // Finally a Post if the state says so
+        if (_state.postId != null)
+          MaterialPage(
+            child: PostDetailsScreen(
+              update: changeAppState,
+              post: posts.firstWhere((element) => element.id == _state.postId),
+            ),
+            name: "post_details",
+          ),
+      ],
+    );
   }
 
   @override
   Future<void> setNewRoutePath(AppState configuration) {
     changeAppState(configuration);
     return Future.value(null);
+  }
+
+  /// Called when the navigator pops a page
+  bool _onPopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+
+    final name = route.settings.name;
+    if (name == "post_details") {
+      changeAppState(const AppState(showPosts: true));
+    } else if (name == "posts") {
+      changeAppState(const AppState());
+    }
+
+    return true;
   }
 
   @override
